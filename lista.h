d637cbeec470ad0e8be82846c2889d80aec53f9b
Alexandre Nassar de Peder <peder@alunos.utfpr.edu.br>
2024-03-12 20:11:07 -0300

    lista em c++

---
+++ lista.h
@@ -0,0 +1,82 @@
+#include <iostream>
+
+class No {
+    friend class Lista;
+
+public:
+    No(int dado){
+        this->dado = dado;
+        this->proximo = nullptr;
+    }
+
+    void imprimir(){
+        std::cout << this->dado;
+    }
+
+private:
+    int dado;
+    No* proximo;
+};
+
+class Lista{
+public:
+    Lista(){
+        //Inicializar a lista vazia
+        this->primeiro = nullptr;
+        this->ultimo = nullptr;
+        this->tamanho = 0;
+    }
+
+/* quando a lista terminar de ser chamada lá no main.cpp ele vai invocar um destrutor que vai destruir só o l do main() do main.cpp
+aqui eu estou alterando o destrutor padrão para percorrer todos os nós livrando a memória e então depois deletar o l do main.cpp
+*/
+    ~Lista(){
+        std::cout << "destruindo a lista\n";
+        No* p = this->primeiro;
+        while (p != nullptr){
+            No* t = p;
+            p = p->proximo;
+            delete t;
+        }
+    }
+
+    bool vazia(){
+        return this->tamanho == 0;
+    }
+
+    //int LST_Inserir(ILISTA* L, int dado);
+    void inserir(int dado){
+        No* novo = new No(dado);
+
+        //Caso 1: a lista é vazia.
+        if(this->vazia()){
+            this->primeiro = novo;
+        }
+        else{
+            //Caso 2: a lista tem pelo menos um elemento.
+            //Ligar o último elemento da lista ao novo elemento e
+            //atualizar o ponteiro para o último elemento.
+            this->ultimo->proximo = novo;
+        }
+
+        this->ultimo = novo;
+        //Atualizar o tamanho da lista
+        this->tamanho++;
+    }
+
+    void imprimir(){
+        for(No* p = this->primeiro;
+            p != nullptr;
+            p = p->proximo){
+
+            p->imprimir();
+            std::cout << ", ";
+        }
+        std::cout << "\n";
+    }
+
+private:
+    int tamanho;
+    No* primeiro;
+    No* ultimo;
+};
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "lista.h"
+
+int main(){
+
+    // No x(5);
+    // No y(10);
+
+    // y.imprimir();
+
+    // No* z = new No(15);
+
+    // z->imprimir();
+
+    // return 0;
+
+    Lista* l = new Lista();
+    l->inserir(4);
+    l->inserir(20);
+    l->inserir(100);
+
+    l->imprimir();
+
+    return 0;
+
+}
